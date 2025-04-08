86e8aec243e5a4d6d8efee9ad301de51fc96e7d0
hyun3560 <metalhyun26@gmail.com>
2025-04-08 19:30:14 +0900

    문장 출력기능 구현2

--- modules/WordSearch/TextDisplayService.cs
+++ modules/WordSearch/TextDisplayService.cs
@@ -4,20 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Documents;
 
 namespace WordSearch
 {
     public class TextDisplayService
     {
-        public static void Display(TextBox box, string message)
+
+        public static void Display(RichTextBox box, string message)
         {
             System.Diagnostics.Debug.WriteLine("[TextDisplayService] 표시할 문장: " + message);
-            box.Text = message;
-        }
-
-        public static void Display(TextBlock block, string message)
-        {
-            block.Text = message;
+            box.Document.Blocks.Clear(); // 기존 내용 제거
+            box.Document.Blocks.Add(new Paragraph(new Run(message))); // 문단에 텍스트 추가
         }
     }
 }
