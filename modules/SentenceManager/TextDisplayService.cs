460a8cf562f42255341bd45a859dfa89424506a2
hyun3560 <metalhyun26@gmail.com>
2025-04-18 22:55:32 +0900

    선택한 문장 번역 기능 구현2
    선택한 문장 번역 기능 구현2

---
+++ modules/SentenceManager/TextDisplayService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace SentenceManager
+{
+    public class TextDisplayService
+    {
+
+        public static void Display(RichTextBox box, string message)
+        {
+            System.Diagnostics.Debug.WriteLine("[TextDisplayService] 표시할 문장: " + message);
+            box.Document.Blocks.Clear(); // 기존 내용 제거
+            box.Document.Blocks.Add(new Paragraph(new Run(message))); // 문단에 텍스트 추가
+        }
+    }
+}
---
+++ modules/Translate/Translator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace Translate
+{
+    public class Translator
+    {
+        public static async Task<string> TranslateAsync(string text)
+        {
+            string exePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "translate.exe");
+
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = exePath,
+                Arguments = $"\"{text}\"",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                StandardOutputEncoding = Encoding.UTF8,
+                StandardErrorEncoding = Encoding.UTF8
+            };
+
+            string result;
+            using (Process process = Process.Start(psi))
+            {
+                result = await process.StandardOutput.ReadToEndAsync();
+                process.WaitForExit();  
+            }
+
+
+            return result.Trim();
+        }
+    }
+}
